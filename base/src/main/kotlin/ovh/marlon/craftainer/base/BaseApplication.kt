5843fd07807d90825816024497052891ffbdbd3c
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2025-08-18 21:20:53 +0200

    add: check in base project if the driver is available

--- base/src/main/kotlin/ovh/marlon/craftainer/base/BaseApplication.kt
+++ base/src/main/kotlin/ovh/marlon/craftainer/base/BaseApplication.kt
@@ -1,9 +1,19 @@
 package ovh.marlon.craftainer.base
 
+import com.github.ajalt.mordant.terminal.info
 import ovh.marlon.craftainer.app.base.AbstractAppBase
+import ovh.marlon.craftainer.sdk.impl.CraftainerClient
 
 class BaseApplication: AbstractAppBase("Base") {
 
+    companion object {
+        val client = CraftainerClient.createUsingSocket()
+    }
+
     override fun start() {
+        if (!client.isDriverAvailable()) {
+            terminal.info("Docker driver is not available. Please ensure you are running this inside a Docker container.")
+            return
+        }
     }
 }
